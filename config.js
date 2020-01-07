b2298241c2680c96c5cca79cfe9a1cb5e75113b9
Mia Dugas <miadugas@outlook.com>
2020-01-07 15:16:12 -0700

    twitget

---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    consumer_key:         '5QzcpO36EKOjtU1JM8JN5ezsm',
+    consumer_secret:      'xerkcXnaU20PROTVzuE5zTbFj40tCzt9ZzkWU1vJ0Pnm7imYkb',
+    access_token:         '1199378415393968128-WMfCAeF2lKoCc3J4MofjmByggvcWqm',
+    access_token_secret:  '3Hwx4TAd4lclYGN9vf8F1G0GyK7XXbenAaCodFwRhXrCT',
+}
+
+// module.exports = {
+//     consumer_key:         'your consumer key goes here',
+//     consumer_secret:      'your consumer secret goes here',
+//     access_token:         'your access token goes here',
+//     access_token_secret:  ' your access token secret goes here ',
+// }
--- script.js
+++ script.js
@@ -1,20 +1,86 @@
 //declare variables, assign to empty values
 var keyword = " ";
-// var country = $('#country').val();
+// var geo = " ";
 var record = 0;
-var startDate = ' ';
-var endDate = ' ';
+// var startDate = ' ';
 
+//require the package
+var Twit = require('twit');
 
+//access my seperate config file with my dev keys
+var config = require('./config');
+console.log(config);
 
-//event listener to listen for the search button
+//authinticate via my config file
+var T = new Twit(config);
+
+//event listener to listen for the search button click
 $('#search').on("click", function(){
-  //on submit, get the values from each input
+
+//on submit, get the values from each input
   var keyword = $('#searchTerm').val();
-  // var country = $('#country').val();
+
+  // how many to retrieve?
   var record = $('#numRecords').val();
-  var startDate = $('#startYear').val();
-  var endDate = $('#endYear').val();
+
+
+  var params = { q: '#searchTerm', count: '#numRecords' };
+
+// the GET
+T.get('search/tweets', params, gotData);
+
+//handle the info
+function gotData(err, data, response) {
+
+  //just GET the raw text
+  var tweets = data.statuses;
+  for (var i = 0; i < tweets.length; i++ ) { 
+  console.log(tweets[i].text);
+  }
+  
+  //the results & display on page
+
+var results = response.response;
+
+var data = $('<p>'+results.docs[i].data + '</p>');
+
+
+  // var headline = $('<h1>'+results.docs[i].headline.main + '</h1>');
+  // var abstract = $('<p>'+results.docs[i].abstract + '</p>');
+  // var url = $('<a href=' + results.docs[i].web_url + '>Link to Article</a>');
+  // $('#topArticles').append(headline, abstract, url);
+
+
+
+  // console.log(data)
+  }
+  
+
+
+
+
+
+
+
+
+
+
+
+
+  // var country = $('#country').val();
+
+  // GeoLocation
+  // var geo = $('#geoLoco').val();
+
+
+
+
+
+  // var endDate = $('#endYear').val();
+  
+  //since when
+  // var startDate = $('#startYear').val();
+
   // var country = $('#country').val();
 
   // var topHeadlines = $('#topHeadlines').val(); 
@@ -27,19 +93,22 @@ $('#search').on("click", function(){
 
 
   //if the start date is equal to 0 (the default state) then save the query URL variable without the date peramters
-  if (startDate.length === 0 && endDate.length === 0){
-    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + keyword + "&api-key=TN6ESlVHihs1cAhsZMjpUi7oMfaZKOUs";
+  // if (startDate.length === 0 
+  //   && endDate.length === 0
+  //   )
+  //   {
+  //   var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + keyword + "&api-key=TN6ESlVHihs1cAhsZMjpUi7oMfaZKOUs";
     // api-key=dab836f7fc564992bdeeaa2c82581978"
-  }else{
+  // }else{
     //else which assumes it has the start and end date, set the query URL to equal with the date perameters
-    queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + keyword + "&begin_date=" + startDate + "0101&end_date=" + endDate + "0101&api-key=TN6ESlVHihs1cAhsZMjpUi7oMfaZKOUs";
-  }
+  //   queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + keyword + "&begin_date=" + startDate + "0101&end_date=" + endDate + "0101&api-key=TN6ESlVHihs1cAhsZMjpUi7oMfaZKOUs";
+  // }
 //AJAX Call
-  $.ajax({
+  // $.ajax({
     //the url is based on the if condition (with or without the date)
-    url: queryURL,
-    method: "GET"
-  }).then(function(response) {
+    // url: queryURL,
+    // method: "GET"
+  // }).then(function(response) {
     //save the initial results full object as a variable. this specific API has an additional response, so it is tapping into the object itself here
 
 //AJAX Call for News API
@@ -59,15 +128,16 @@ $('#search').on("click", function(){
 
 
 
-    var results = response.response;
+    // var results = response.response;
     //run a for loop. while i is less than the amount of records the user requested
-    for (var i = 0; i < record; i++){
+    // for (var i = 0; i < record; i++){
       //the headline equals the results headline, abstract, url etc.
-      var headline = $('<h1>'+results.docs[i].headline.main + '</h1>');
-      var abstract = $('<p>'+results.docs[i].abstract + '</p>');
-      var url = $('<a href=' + results.docs[i].web_url + '>Link to Article</a>');
-      $('#topArticles').append(headline, abstract, url);
-    }
-});
-});
-
+      // var headline = $('<h1>'+results.docs[i].headline.main + '</h1>');
+      // var abstract = $('<p>'+results.docs[i].abstract + '</p>');
+      // var url = $('<a href=' + results.docs[i].web_url + '>Link to Article</a>');
+      // $('#topArticles').append(headline, abstract, url);
+//     }
+// });
+// });
+}
+)
